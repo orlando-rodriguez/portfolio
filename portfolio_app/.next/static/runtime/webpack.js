e94a02d350be50fb628f5c8e6e537a2656ac048b
Orlando Rodriguez <orlando.r@me.com>
2018-10-25 16:32:58 -0600

    new build script

--- portfolio_app/.next/static/runtime/webpack.js
+++ portfolio_app/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "edeaaaeb6b48d9c8c97e";
+/******/ 	var hotCurrentHash = "e3692321f01513922ffe";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
